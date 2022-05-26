7c68b781313dfbb27cdd9c6cebc5f5e9b00feecc
Maulanaaryo <Maulanaaryo151@gmail.com>
2022-05-26 22:33:23 +0700

    menambahkan warna dan fonts

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const Color primaryColor = Color(0xFF086972);
 const Color whiteColor70 = Color(0xB3FFFFFF);
 const Color whiteColor = Color(0xFFFFFFFF);
+const Color greenColor = Color(0xFF17B978);
+const Color lightGreenColor = Color(0xFFA7FF83);
+const Color darkGreenColor = Color(0xFF086972);
+const Color darkPurpleColor = Color(0xFF071A52);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+TextStyle white70TextyStyle = GoogleFonts.poppins(
+  color: whiteColor70,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor,
+);
+TextStyle lightGreenTextStyle = GoogleFonts.poppins(
+  color: lightGreenColor,
+);
+TextStyle darkGreenTextStyle = GoogleFonts.poppins(
+  color: darkGreenColor,
+);
+TextStyle darkPurpleTextStyle = GoogleFonts.poppins(
+  color: darkPurpleColor,
+);
+
+FontWeight light = FontWeight.w100;
+FontWeight regular = FontWeight.w300;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w900;
