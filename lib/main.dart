4c5fd22730776063c3429bf0c335575b6b7294be
reaperizy <wadi.wahyudin7@gmail.com>
2022-06-13 10:23:42 +0700

    merubah tittle + label(android xml) pada aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'KWI App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
